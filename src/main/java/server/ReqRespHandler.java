91fc95b7d15075427b69af783c4f8c321117822e
giorgiopea <annatar93@gmail.com>
2017-08-20 22:52:46 +0200

    fixed chat shape

--- src/main/java/server/ReqRespHandler.java
+++ src/main/java/server/ReqRespHandler.java
@@ -145,7 +145,6 @@ public class ReqRespHandler extends Thread {
                 this.closeConnection(this.socket, this.objectOutputStream, this.objectInputStream);
                 return;
             }
-            //this.sendRequest(game.getLastRRclientNotification(),this.objectOutputStream);
             this.closeConnection(this.socket, this.objectOutputStream, this.objectInputStream);
         } else if (request.getActionIdentifier().equals(EncodedBehaviourIdentifiers.publishChatMsg())) {
             String message = (String) request.getParameters().get(0);
--- src/main/java/server_side_policies/GamePostChatMsgSidePolicy.java
+++ src/main/java/server_side_policies/GamePostChatMsgSidePolicy.java
@@ -1,6 +1,7 @@
 package server_side_policies;
 
 import common.PSNotification;
+import common.Player;
 import server.Game;
 import server.Helpers;
 import server.PubSubHandler;
@@ -15,12 +16,18 @@ public class GamePostChatMsgSidePolicy implements SidePolicy {
     public void apply(ServerState state, StoreAction action) {
         GamePostMsgAction castedAction = (GamePostMsgAction) action;
         ServerStore SERVER_STORE = ServerStore.getInstance();
-        PSNotification notification = new PSNotification(castedAction.getMessage(), null,null,
-                false,false,null, null, false,false,false,false, null);
-        Game game = Helpers.findGameById(castedAction.getPlayerToken().getGameId(),SERVER_STORE.getState().getGames());
+
+        Game game = Helpers.findGameById(castedAction.getPlayerToken().getGameId(),state.getGames());
         if (game == null){
             return;
         }
+        Player player = Helpers.findPlayerByToken(castedAction.getPlayerToken(),game.getPlayers());
+        if (player == null){
+            return;
+        }
+        PSNotification notification = new PSNotification("["+player.getName()+"]:"+castedAction.getMessage(), null,null,
+                false,false,null, null, false,false,false,false, null);
+
         for (PubSubHandler handler : game.getPubSubHandlers()){
             handler.queueNotification(notification);
         }
